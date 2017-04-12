753e0b15b7990c7c6e075089136a5e49bfa4e0f0
raj0708 <rajesh.bura90@gmail.com>
2017-04-12 15:02:40 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,22 +1,45 @@
 <?php
-//Fetching values from url
-//database name = "mydata"
-//table name = "streetviewdata"
 
-$connection = mysql_connect("localhost", "root", "");
-if(!$connection)
-{
-	die("could not connect: ".mysql_error());
+//$database name = "mydata"
+//$table name = "streetviewdata"
+//$servername = "localhost";
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "mydata");
+
+//$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+$mysqli = new mysqli("localhost", "root", "", "mydata");
+
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
 }
-$db = mysql_select_db("mydata", $connection);
-$result = mysql_query("select ID, ImgLocation, ImagePath, Latitude, Longitude, CardinalDir_Lat, CardinalDir_Lng, Vertlimit_min, Vertlimit_max 
-					  from streetviewdata where id={$_POST["ID"]}", $connection);
-//$id = {$_POST["ID"]};
-echo json_encode(mysql_fetch_assoc($result));
 
-mysql_free_result($result);
+$uid = $_POST["ID"];
+//$query = "select * from streetviewdata where uid=?";
+$query = "select ID, ImgLocation, ImagePath, Latitude, Longitude, CardinalDir_Lat, CardinalDir_Lng, Vertlimit_min, Vertlimit_max 
+				  from streetviewdata where id=?"; 
 
-mysql_close($connection);
+if($stmt = $mysqli->prepare($query)){
+
+$userid = $uid;
+$stmt->bind_param('i', $userid);
+$stmt->execute();
+$result = $stmt->get_result();
+		echo json_encode(mysqli_fetch_assoc($result));
+		mysqli_free_result($result);
+
+}
+else{
+	
+  	var_dump($mysqli->error);
+}
+		
+$mysqli->close();
+
 ?>
 
 
